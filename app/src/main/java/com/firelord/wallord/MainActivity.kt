82ff8a6998794dbcbfce238919b2d41fb578ba0c
FireLord1 <firelord.xda@lineageos.org>
2018-04-03 00:06:56 +0530

    wallord: Implement bottom navbar
    Signed-off-by: FireLord1 <firelord.xda@lineageos.org>

--- app/src/main/java/com/firelord/wallord/MainActivity.kt
+++ app/src/main/java/com/firelord/wallord/MainActivity.kt
@@ -1,69 +1,45 @@
 package com.firelord.wallord
 
 import android.os.Bundle
-import android.support.design.widget.NavigationView
-import android.support.v4.view.GravityCompat
-import android.support.v7.app.ActionBarDrawerToggle
+import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
-import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.app_bar_main.*
 import com.stephentuso.welcome.WelcomeHelper
 
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class MainActivity : AppCompatActivity() {
 
     private var welcomeScreen: WelcomeHelper? = null
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                message.setText(R.string.title_home)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_gallery-> {
+                message.setText(R.string.title_gallery)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_favourite -> {
+                message.setText(R.string.title_favourite)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
 
         welcomeScreen = WelcomeHelper(this, AppIntroActivity::class.java)
         welcomeScreen!!.show(savedInstanceState)
 
-        val toggle = ActionBarDrawerToggle(
-                this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
-        drawer_layout.addDrawerListener(toggle)
-        toggle.syncState()
-
-        nav_view.setNavigationItemSelectedListener(this)
-    }
-
-    override fun onBackPressed() {
-        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
-            drawer_layout.closeDrawer(GravityCompat.START)
-        } else {
-            super.onBackPressed()
-        }
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
 
     override fun onSaveInstanceState(outState: Bundle?) {
         super.onSaveInstanceState(outState)
         welcomeScreen!!.onSaveInstanceState(outState)
-    }
-
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        // Handle navigation view item clicks here.
-        when (item.itemId) {
-            R.id.nav_home -> {
-                // Handle the camera action
-            }
-            R.id.nav_gallery -> {
-
-            }
-            R.id.nav_favourite -> {
-
-            }
-            R.id.nav_share -> {
-
-            }
-            R.id.nav_feedback -> {
-
-            }
-        }
-
-        drawer_layout.closeDrawer(GravityCompat.START)
-        return true
     }
 }
