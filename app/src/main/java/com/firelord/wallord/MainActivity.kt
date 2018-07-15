192d156f2b44fe4146b92a204d29ec0f51480f35
Shubham Singh <coolsks94@gmail.com>
2018-07-16 02:33:34 +0530

    Switch to ahbottomnavigation library
     * also create dummy fragments for the available tabs

--- app/src/main/java/com/firelord/wallord/MainActivity.kt
+++ app/src/main/java/com/firelord/wallord/MainActivity.kt
@@ -1,31 +1,48 @@
 package com.firelord.wallord
 
+import android.app.Fragment
 import android.os.Bundle
-import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import com.aurelhubert.ahbottomnavigation.AHBottomNavigationAdapter
+import com.firelord.wallord.fragments.AllPhotosFragment
+import com.firelord.wallord.fragments.HomeFragment
+import com.firelord.wallord.fragments.SupportFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_home -> {
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_dashboard -> {
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
-    }
+    private lateinit var navigationAdapter: AHBottomNavigationAdapter
+    private var fragment:Fragment = HomeFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        navigationAdapter = AHBottomNavigationAdapter(this,R.menu.navigation)
+        navigationAdapter.setupWithBottomNavigation(bottom_navigation)
+
+        bottom_navigation.setOnTabSelectedListener({ position, wasSelected ->
+
+            when(position){
+                0 -> fragment = HomeFragment()
+                1 -> fragment = AllPhotosFragment()
+                2 -> fragment = SupportFragment()
+            }
+
+            gotoFragment(fragment)
+            true
+        })
+
+        if (savedInstanceState == null) {
+
+            gotoFragment(fragment) // On activity creation load first tab
+        }
+
+    }
+
+    private fun gotoFragment(fragment: Fragment){
+        val ft = fragmentManager.beginTransaction()
+        ft.replace(R.id.fragment_frame, fragment)
+        ft.commit()
     }
 }
